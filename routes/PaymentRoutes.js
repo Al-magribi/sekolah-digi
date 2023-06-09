94936153051e32538faff440071b00c35a1c4e58
Mujaddid Al Magribi <almagribi.appdev@gmail.com>
2023-06-09 17:49:59 +0700

    Konfigurasi Payment

--- routes/PaymentRoutes.js
+++ routes/PaymentRoutes.js
@@ -16,7 +16,7 @@ router.post("/v1/notification", async (req, res) => {
   const core = new midtransClient.CoreApi();
 
   core.apiConfig.set({
-    isProduction: true,
+    isProduction: process.env.PRODUCTION,
     serverKey: process.env.SERVER_KEY,
     clientKey: process.env.CLIENT_KEY,
   });
@@ -74,63 +74,67 @@ router.post("/v1/notification", async (req, res) => {
 
 // HANDLE PEMBAYARAN DENGAN MIDTRANS
 router.post("/transaction", (req, res) => {
-  let snap = new midtransClient.Snap({
-    isProduction: true,
-    serverKey: process.env.SERVER_KEY,
-    clientKey: process.env.CLIENT_KEY,
-  });
-
-  let parameter = {
-    transaction_details: {
-      order_id: req.body.order_id,
-      gross_amount: req.body.payment,
-    },
-    credit_card: {
-      secure: true,
-    },
-    customer_details: {
-      first_name: req.body.name,
-      email: req.body.email,
-    },
-    callbacks: {
-      finish: "http://localhost:3000/payment/status",
-    },
-    enabled_payments: [
-      "credit_card",
-      "mandiri_clickpay",
-      "bca_klikbca",
-      "bca_klikpay",
-      "echannel",
-      "permata_va",
-      "bca_va",
-      "bni_va",
-      "other_va",
-    ],
-  };
-
-  snap
-    .createTransaction(parameter)
-    .then((transaction) => {
-      const dataPayment = {
-        midtransResponse: JSON.stringify(transaction),
-      };
-
-      let transactionToken = transaction.token;
-
-      res.status(200).json({
-        status: true,
-        message: "berhasil",
-        dataPayment,
-        token: transactionToken,
-      });
-    })
-    .catch((e) => {
-      res.status(400).json({
-        status: false,
-        message: "Pembayaran gagal",
-        error: e.message,
-      });
+  try {
+    let snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.SERVER_KEY,
+      clientKey: process.env.CLIENT_KEY,
     });
+
+    let parameter = {
+      transaction_details: {
+        order_id: req.body.order_id,
+        gross_amount: req.body.payment,
+      },
+      credit_card: {
+        secure: true,
+      },
+      customer_details: {
+        first_name: req.body.name,
+        email: req.body.email,
+      },
+      callbacks: {
+        finish: "http://localhost:3000/payment/status",
+      },
+      enabled_payments: [
+        "credit_card",
+        "mandiri_clickpay",
+        "bca_klikbca",
+        "bca_klikpay",
+        "echannel",
+        "permata_va",
+        "bca_va",
+        "bni_va",
+        "other_va",
+      ],
+    };
+
+    snap
+      .createTransaction(parameter)
+      .then((transaction) => {
+        const dataPayment = {
+          midtransResponse: JSON.stringify(transaction),
+        };
+
+        let transactionToken = transaction.token;
+
+        res.status(200).json({
+          status: true,
+          message: "berhasil",
+          dataPayment,
+          token: transactionToken,
+        });
+      })
+      .catch((e) => {
+        res.status(400).json({
+          status: false,
+          message: "Pembayaran gagal",
+          error: e.message,
+        });
+      });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // MEMPERBARUI STATUS PEMBAYARAN DENGAN MINTA RESPONSE DARI MIDTRANS
@@ -139,7 +143,7 @@ router.get(
   authenticateToken,
   AsyncError(async (req, res) => {
     let snap = new midtransClient.Snap({
-      isProduction: true,
+      isProduction: process.env.PRODUCTION,
       serverKey: process.env.SERVER_KEY,
       clientKey: process.env.CLIENT_KEY,
     });
